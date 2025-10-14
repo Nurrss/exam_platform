9cea7047fd2c3f711e8ff8ebb600434297a82552
Nursultan <nurrs.serkul@gmail.com>
2025-10-14 21:07:57 +0500

    register email feat was created

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -4,9 +4,12 @@ const authController = require('../controllers/authController');
 const authMiddleware = require('../middleware/authMiddleware');
 
 router.post('/register', authController.register);
+router.get('/verify-email/:token', authController.verifyEmail);
 router.post('/login', authController.login);
 router.post('/refresh', authController.refresh);
 router.post('/logout', authMiddleware, authController.logout);
-router.get('/me', authMiddleware, authController.getCurrentUser);
+router.post('/change-password', authMiddleware, authController.changePassword);
+router.post('/forgot-password', authController.forgotPassword);
+router.post('/reset-password', authController.resetPassword);
 
 module.exports = router;
