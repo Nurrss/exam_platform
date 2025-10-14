c3ffe4cfbbd0443879802bbb91bf5e60755b4676
Nursultan <nurrs.serkul@gmail.com>
2025-10-14 21:28:35 +0500

    update user routes

--- src/repositories/userRepository.js
+++ src/repositories/userRepository.js
@@ -1,43 +1,38 @@
 const prisma = require('../config/prismaClient');
-const bcrypt = require('bcrypt');
 
-class UserRepository {
-  async getAll() {
-    try {
-      return await prisma.user.findMany({
-        select: {
-          id: true,
-          email: true,
-          role: true,
-          createdAt: true,
-        },
-      });
-    } catch (err) {
-      console.error('❌ Error in getAllUsers:', err);
-      throw new Error('Ошибка при получении пользователей');
-    }
-  }
+exports.getAll = () => {
+  return prisma.user.findMany({
+    orderBy: { createdAt: 'desc' },
+  });
+};
 
-  async create(data) {
-    try {
-      const hashedPassword = await bcrypt.hash(data.password, 10);
-      return await prisma.user.create({
-        data: { ...data, password: hashedPassword },
-      });
-    } catch (err) {
-      console.error('❌ Error in createUser:', err);
-      throw new Error('Ошибка при создании пользователя');
-    }
-  }
+exports.getById = (id) => {
+  return prisma.user.findUnique({
+    where: { id: Number(id) },
+  });
+};
 
-  async getByEmail(email) {
-    try {
-      return await prisma.user.findUnique({ where: { email } });
-    } catch (err) {
-      console.error('❌ Error in getByEmail:', err);
-      throw new Error('Ошибка при поиске пользователя по email');
-    }
-  }
-}
+exports.create = (data) => {
+  return prisma.user.create({ data });
+};
 
-module.exports = new UserRepository();
+exports.update = (id, data) => {
+  return prisma.user.update({
+    where: { id: Number(id) },
+    data,
+    select: {
+      id: true,
+      firstName: true,
+      lastName: true,
+      email: true,
+      role: true,
+      updatedAt: true,
+    },
+  });
+};
+
+exports.delete = (id) => {
+  return prisma.user.delete({
+    where: { id: Number(id) },
+  });
+};
